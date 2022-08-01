874802f3cc6a35d9afb54271a99f76c43565b294
William Hearn <william.hearn@canada.ca>
2022-08-01 11:52:00 -0400

    feat(kubecost): Add required firewall rule

--- 00-network.tf
+++ 00-network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/statcan/terraform-azure-statcan-aaw-network.git?ref=v0.3.12"
+  source = "git::https://github.com/statcan/terraform-azure-statcan-aaw-network.git?ref=v0.3.13"
 
   prefix             = local.prefix
   location           = var.azure_region
