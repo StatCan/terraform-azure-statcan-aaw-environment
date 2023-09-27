42dff38d843e04aa6d4c0973cb3aa19701495bf7
Justin Bertrand <justin.bertrand@canada.ca>
2023-09-27 19:47:32 +0000

    fix: update module to fix permission.

--- 01-infrastructure.tf
+++ 01-infrastructure.tf
@@ -1,5 +1,5 @@
 module "infrastructure" {
-  source = "git::https://github.com/statcan/terraform-statcan-azure-cloud-native-environment-infrastructure.git?ref=v1.0.6"
+  source = "git::https://github.com/statcan/terraform-statcan-azure-cloud-native-environment-infrastructure.git?ref=v1.0.7"
 
   prefix   = local.prefix
   tags     = local.azure_tags
